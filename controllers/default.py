b809ab8c18eba6ffd96d828c9e9bccf0aa251a7e
praveen <mailmeonlypraveen@gmail.com>
2015-11-12 17:53:01 +0530

    no change

--- controllers/default.py
+++ controllers/default.py
@@ -3,9 +3,17 @@ def index():
     rows = db(db.courier).select()
     return locals()
 
+@auth.requires_login()#create can only be called by logged in people
 def create():
     #create own form in index.html
+    db.courier.arrival_date.default = request.now
+    db.courier.arrival_date.writable = False
+    db.courier.arrival_date.readable = False
+    db.courier.last_email_sent.default = request.now
+    db.courier.last_email_sent.writable = False
+    db.courier.last_email_sent.readable = False
     form = SQLFORM(db.courier).process()
+    if form.accepted: redirect(URL('index'))
     return locals()
 
 def show():
